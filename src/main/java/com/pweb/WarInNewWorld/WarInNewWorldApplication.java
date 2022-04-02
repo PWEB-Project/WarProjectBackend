a5e96daf3a779ae569506b96b41b22ffdd9a77bc
stela.josan <stela.josan@stud.acs.upb.ro>
2022-04-02 16:16:09 +0300

    The spring initiated

---
+++ src/main/java/com/pweb/WarInNewWorld/WarInNewWorldApplication.java
@@ -0,0 +1,13 @@
+package com.pweb.WarInNewWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarInNewWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarInNewWorldApplication.class, args);
+	}
+
+}
