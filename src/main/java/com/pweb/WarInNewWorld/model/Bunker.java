bfb808ed41c20bc57f5eae9044ac95a2b6ce9d07
stela.josan <stela.josan@stud.acs.upb.ro>
2022-04-23 12:54:43 +0300

    Buncker made

--- src/main/java/com/pweb/WarInNewWorld/model/Bunker.java
+++ src/main/java/com/pweb/WarInNewWorld/model/Bunker.java
@@ -1,4 +1,23 @@
 package com.pweb.WarInNewWorld.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pwb_bunkers")
 public class Bunker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name  = "bunker_id")
+    private Long id;
+
+    @Column(name = "buncker_address")
+    private String address;
+
+    @Column(name = "maxim_capacity")
+    private Integer maximCapacity;
+
+    @Column(name = "current_capacity")
+    private Integer currentCapacity;
+
+
 }
